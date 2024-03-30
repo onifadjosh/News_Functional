0728bf1ad94097f921cab8aaa9d73c033a25ebde
Onifade Joshua <onifadjosh@gmail.com>
2024-03-30 10:05:52 +0100

    Navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="">
+    <div className="bg-[#ECECEC]">
       <Nav></Nav>
     </div>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,16 +3,22 @@ import inklLogo from '.././images/inkl-logo.svg';
 
 const Nav = () => {
   return (
-    <div>
+    <div className='px-[20px] py-[20px] flex justify-between items-center'>
         <img src={inklLogo} alt="" height={40} width={80}/>
 
         {/* link to be routed later on */}
-        <a href="http://">Sources</a>
-        <a href="http://">Pricing</a>
-        <a href="http://">Download</a>
+        <div className='flex gap-[24px] items-center'>
+            <span className='flex gap-[24px] font-medium text-[20px] leading-[28px] text-[#787878]'>
+                <a href="http://">Sources</a>
+                <a href="http://">Pricing</a>
+                <a href="http://">Download</a>
+            </span>
 
-        <a href="h" className=''>Sign In</a>
-        <a href="ht">Sign Up</a>
+            <span className='flex gap-[16px]'>
+                <a href="h" className='px-[40px] py-[8px] bg-[#141E28] text-[#FFFFFF] rounded-[9999px] font-medium text-[20px] leading-[28px]'>Sign In</a>
+                <a href="ht" className='px-[40px] py-[8px] bg-[#ED5503] text-[#FFFFFF] rounded-[9999px] font-medium text-[20px] leading-[28px]'>Sign Up</a>
+            </span>
+        </div>
     </div>
   )
 }
