4eea2f0a6a83b99bc01a4e7c9eb326c1b5457e52
Onifade Joshua <onifadjosh@gmail.com>
2024-04-15 10:29:56 +0100

    updates on 15/4

--- src/components/GroupNavigation.jsx
+++ src/components/GroupNavigation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GroupNavigation = () => {
   return (
-    <div className='flex justify-between px-[20px] py-[24px] border-y-[1px] border-[#ECECEC] text-[24px] leading-[32px] text-[141E28] font-normal'>
+    <div className='flex justify-between /px-[192px] px-[24px] py-[24px] border-y-[1px] border-[#ECECEC] text-[16px] leading-[32px] text-[141E28] font-normal'>
       <a href="h">Lead Stories</a>
       <a href="h">Good News</a>
       <a href="h">Our Picks</a>
